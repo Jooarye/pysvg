a7da0ff1a347ad0dc43843842140eb9700edfdfb
Jooarye <jooarye@outlook.com>
2019-09-23 10:56:03 +0200

    Documentation

--- __init__.py
+++ __init__.py
@@ -2,6 +2,10 @@ from typing import List, Dict, Union, Tuple
 
 
 class SVGObject:
+    """
+    General svg object. All further items are instances of this class!
+    """
+
     def __init__(self, **kwargs):
         self.parameters = {}
         for k, v in kwargs.items():
@@ -9,16 +13,31 @@ class SVGObject:
                 self.parameters[k.replace('_', '-')] = v
 
     def finalize(self):
+        """
+        Function used to form the final svg tag.
+        :return: The svg tag
+        """
         return ''
 
     def get_prefix(self):
+        """
+        Function used to get the svg tag prefix
+        :return: The svg tag prefix
+        """
         return ''
 
     def get_suffix(self):
+        """
+        Function used to get the svg tag suffix (Only for opentags)
+        :return: The svg suffix
+        """
         return ''
 
 
 class SVGClosedObject(SVGObject):
+    """
+    General svg object, used to represent all closed tags
+    """
     def __init__(self, **kwargs):
         super(SVGClosedObject, self).__init__(**kwargs)
 
@@ -31,15 +50,23 @@ class SVGClosedObject(SVGObject):
 
 
 class SVGOpenObject(SVGObject):
-    svg_objects: List[SVGObject] = []
-
+    """
+    General svg object, used to represent all open tags!
+    """
     def __init__(self, **kwargs):
+        svg_objects = []
         super(SVGOpenObject, self).__init__(**kwargs)
 
     def add(self, svg_object: SVGObject):
+        """
+        Add item to open object.
+        """
         self.svg_objects.append(svg_object)
 
     def rem(self, svg_index: int):
+        """
+        Remove item at index from open object.
+        """
         if len(self.svg_objects) >= svg_index:
             del self.svg_objects[svg_index]
 
@@ -48,6 +75,9 @@ class SVGOpenObject(SVGObject):
 
 
 class SVGGraphic(SVGOpenObject):
+    """
+    General class to represent a svg graphic.
+    """
     def get_prefix(self):
         return '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'
 
@@ -55,12 +85,20 @@ class SVGGraphic(SVGOpenObject):
         return '</svg>'
 
     def save(self, file):
+        """
+        Function used to save the svg to a file
+        :param file: The file to write to.
+        :type file: open(...)
+        """
         temp = "".join([svg_object.finalize() for svg_object in self.svg_objects])
         file.write(f'{self.get_prefix()}{temp}{self.get_suffix()}')
         file.flush()
 
 
 class SVGCircle(SVGClosedObject):
+    """
+    General svg object, used to represent a circle
+    """
     def __init__(self, cx: float, cy: float, r: float, **kwargs):
         kwargs['cx'] = cx
         kwargs['cy'] = cy
@@ -72,6 +110,9 @@ class SVGCircle(SVGClosedObject):
 
 
 class SVGRect(SVGClosedObject):
+    """
+    General svg object, used to represent a rectangle.
+    """
     def __init__(self, pos: Tuple[float, float], dim: Tuple[float, float], **kwargs):
         kwargs['x'], kwargs['y'] = pos
         kwargs['width'], kwargs['height'] = dim
@@ -82,6 +123,9 @@ class SVGRect(SVGClosedObject):
 
 
 class SVGPolygon(SVGClosedObject):
+    """
+    General svg object, used to represent a polygon.
+    """
     def __init__(self, points: List[Tuple[float, float]], **kwargs):
         kwargs['points'] = " ".join([f"{x},{y}" for x, y in points])
         super(SVGPolygon, self).__init__(**kwargs)
@@ -91,6 +135,9 @@ class SVGPolygon(SVGClosedObject):
 
 
 class SVGLine(SVGClosedObject):
+    """
+    General svg object, used to represent a line.
+    """
     def __init__(self, start: Tuple[float, float], end: Tuple[float, float], **kwargs):
         kwargs['x1'], kwargs['y1'] = start
         kwargs['x2'], kwargs['y2'] = end
